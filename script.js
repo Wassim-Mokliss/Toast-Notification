fb23c9dd77921822d0c69f31ebda6a5de1bb1c54
Wassim <wasshimu.dev@gmail.com>
2024-12-03 04:00:27 +0100

    Ajout ecouteur evenement sur bouton + creation via dom div + timeout

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('connecté !')
+console.log('connecté !');
+// Je selctionne et je stock le bouton
+const btn = document.querySelector('.btn');
+// Je selectionne et stock la div notification
+const ctn = document.querySelector('.container-notifications');
+console.log(ctn);
+
+btn.addEventListener('click', function(){
+    console.log('clické');
+    const notification = document.createElement('div');
+    console.log(notification);
+    notification.classList.add('toast');
+    notification.innerText = ('Votre fichier est bien enregistré !');
+    ctn.appendChild(notification);
+    setTimeout(function(){
+        notification.remove();
+    }, 2000)
+});
